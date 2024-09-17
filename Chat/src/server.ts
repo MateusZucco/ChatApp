9ebbc7f02e4f653b42c4ad5451ea25f792ae432d
Zucco <zucco.developer@gmail.com>
2024-09-16 21:52:34 -0300

    feat(connection): create database connection

--- Chat/src/server.ts
+++ Chat/src/server.ts
@@ -1,7 +1,10 @@
 import { httpServer } from './http';
 import './webSocket';
+// import { Connection } from './connection';
 
+// const connection = new Connection();
 
 httpServer.listen(process.env.PORT || 8080, () => {
+  // connection.connect();
   console.log(`Server is running, on port ${process.env.PORT || 8080} `);
 });
